ba20e2034b33534648b3d0fccb4dda1b5b7294ba
Lorraine Nkuna <49310410@mylife.unisa.ac.za>
2016-02-21 23:54:01 +0200

    Start of project

---
+++ Athletic Club Management System/Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainMenu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtEvent_Name = New System.Windows.Forms.TextBox()
+        Me.txtEvent_Reg_Fee = New System.Windows.Forms.TextBox()
+        Me.dtpEvent_Date = New System.Windows.Forms.DateTimePicker()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.txtEvent_Location = New System.Windows.Forms.TextBox()
+        Me.cbxEvent_distance = New System.Windows.Forms.ComboBox()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Location = New System.Drawing.Point(40, 30)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(685, 521)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.cbxEvent_distance)
+        Me.TabPage1.Controls.Add(Me.txtEvent_Location)
+        Me.TabPage1.Controls.Add(Me.btnClear)
+        Me.TabPage1.Controls.Add(Me.btnDelete)
+        Me.TabPage1.Controls.Add(Me.btnUpdate)
+        Me.TabPage1.Controls.Add(Me.btnAdd)
+        Me.TabPage1.Controls.Add(Me.dtpEvent_Date)
+        Me.TabPage1.Controls.Add(Me.txtEvent_Reg_Fee)
+        Me.TabPage1.Controls.Add(Me.txtEvent_Name)
+        Me.TabPage1.Controls.Add(Me.Label6)
+        Me.TabPage1.Controls.Add(Me.Label5)
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(677, 495)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Manage Events"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(677, 495)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Manage Athletes"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Size = New System.Drawing.Size(677, 495)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Manage Results"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(246, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(120, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Manage Athletic Events"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(40, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Event Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(40, 180)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Event Date"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(40, 238)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(84, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Registration Fee"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(40, 309)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(79, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Event Location"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(40, 376)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Distance"
+        '
+        'txtEvent_Name
+        '
+        Me.txtEvent_Name.Location = New System.Drawing.Point(302, 100)
+        Me.txtEvent_Name.Name = "txtEvent_Name"
+        Me.txtEvent_Name.Size = New System.Drawing.Size(200, 20)
+        Me.txtEvent_Name.TabIndex = 6
+        '
+        'txtEvent_Reg_Fee
+        '
+        Me.txtEvent_Reg_Fee.Location = New System.Drawing.Point(302, 231)
+        Me.txtEvent_Reg_Fee.Name = "txtEvent_Reg_Fee"
+        Me.txtEvent_Reg_Fee.Size = New System.Drawing.Size(200, 20)
+        Me.txtEvent_Reg_Fee.TabIndex = 7
+        '
+        'dtpEvent_Date
+        '
+        Me.dtpEvent_Date.Location = New System.Drawing.Point(302, 172)
+        Me.dtpEvent_Date.Name = "dtpEvent_Date"
+        Me.dtpEvent_Date.Size = New System.Drawing.Size(200, 20)
+        Me.dtpEvent_Date.TabIndex = 8
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(129, 446)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 9
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(238, 446)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.TabIndex = 10
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(357, 446)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 11
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(468, 446)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 12
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'txtEvent_Location
+        '
+        Me.txtEvent_Location.Location = New System.Drawing.Point(302, 302)
+        Me.txtEvent_Location.Name = "txtEvent_Location"
+        Me.txtEvent_Location.Size = New System.Drawing.Size(200, 20)
+        Me.txtEvent_Location.TabIndex = 13
+        '
+        'cbxEvent_distance
+        '
+        Me.cbxEvent_distance.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbxEvent_distance.FormattingEnabled = True
+        Me.cbxEvent_distance.Items.AddRange(New Object() {"5KM", "10KM", "21KM", "42KM", "50KM", "90KM"})
+        Me.cbxEvent_distance.Location = New System.Drawing.Point(302, 367)
+        Me.cbxEvent_distance.Name = "cbxEvent_distance"
+        Me.cbxEvent_distance.Size = New System.Drawing.Size(121, 21)
+        Me.cbxEvent_distance.TabIndex = 14
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(306, 576)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(133, 23)
+        Me.btnClose.TabIndex = 1
+        Me.btnClose.Text = "Close "
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'MainMenu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(749, 651)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "MainMenu"
+        Me.Text = "Main Menu"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents txtEvent_Reg_Fee As System.Windows.Forms.TextBox
+    Friend WithEvents txtEvent_Name As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage3 As System.Windows.Forms.TabPage
+    Friend WithEvents cbxEvent_distance As System.Windows.Forms.ComboBox
+    Friend WithEvents txtEvent_Location As System.Windows.Forms.TextBox
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnUpdate As System.Windows.Forms.Button
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents dtpEvent_Date As System.Windows.Forms.DateTimePicker
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+
+End Class
---
+++ Athletic Club Management System/Form1.vb
@@ -0,0 +1,72 @@
+﻿
+Imports System.IO                               'references to the System Input Output classes
+Public Class MainMenu
+    Dim dir As String = "D:\VB 2012\Files\"     'reference to the directory where file is stored
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If Not Directory.Exists(dir) Then       'check if the directory exists, if not it is created
+            Directory.CreateDirectory(dir)
+        End If
+
+        Dim path As String = dir & " Events.txt" 'check if file is available, if so it deletes it
+        If File.Exists(path) Then
+            ' File.Delete(path)
+        End If
+        Dim fs As New FileStream(path, FileMode.Create, FileAccess.Write) 'filestream for writing
+
+        Try
+            fs = New FileStream(path, FileMode.Open) 'filestream for writing
+            Dim textOut As New StreamWriter(fs)
+
+            For Each event as Event in events
+                textOut.Write(event
+
+        Catch ex As FileNotFoundException
+            MessageBox.Show(path & " not found.", "File Not Found")
+        Catch ex As DirectoryNotFoundException
+            MessageBox.Show(dir & " not found.", "Directory Not Found")
+        Catch ex As IOException
+            MessageBox.Show(ex.Message, "IOException")
+        Finally
+            If fs IsNot Nothing Then
+                fs.Close()
+            End If
+        End Try
+
+
+        Dim eventName As Char
+        Dim eventDate As Date
+        Dim eventLocation As Char
+        Dim regFee As Integer
+        Dim eventDistance As Char
+
+        eventName = txtEvent_Name.Text
+        eventDate = dtpEvent_Date.Value
+        eventLocation = txtEvent_Location.Text
+        regFee = txtEvent_Reg_Fee.Text
+        eventDistance = cbxEvent_distance.SelectedItem
+
+        txtEvent_Location.Text = eventDistance
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtEvent_Name.Text = ""
+        dtpEvent_Date.Refresh()
+        txtEvent_Reg_Fee.Text = ""
+        txtEvent_Location.Clear()
+        cbxEvent_distance.ResetText()
+    End Sub
+
+    Private Sub TabPage1_Click(sender As Object, e As EventArgs) Handles TabPage1.Click
+
+    End Sub
+End Class
